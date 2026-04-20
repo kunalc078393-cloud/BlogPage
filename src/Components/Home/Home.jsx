f50121591283792605e5c3955a3f5a94ff197a54
kunalc078393-cloud <kunalc078393@gmail.com>
2026-04-20 22:06:36 +0530

    Update image source in Home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,7 +10,7 @@ export default function Home() {
 
                     <img
                         className="w-full h-full object-cover"
-                        src=""
+                        src="https://images.pexels.com/photos/33579599/pexels-photo-33579599.jpeg"
                         alt=""
                     />
                 </div>
